43de30cabf726224b0c76a5dcdd5eae61de793f8
Jhon Ochoa <jhon.ochoa@openintl.com>
2024-12-07 11:31:30 -0500

    add test cases fix 3

--- ejercicio_total_test.py
+++ ejercicio_total_test.py
@@ -2,11 +2,10 @@ from ejercicios import total
 import unittest
 
 class TestCalcularTotalTest(unittest.TestCase):
-    def test_1_calcular(self):
-
+    def test_1(self):
         lst=[1,2,3,4]
         resultado = total(lst)
-        self.assertEqual(2, resultado)
+        self.assertEqual(11, resultado)
 
 if __name__ == '__main__':
     unittest.main()
