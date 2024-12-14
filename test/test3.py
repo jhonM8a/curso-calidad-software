da8f69e1e633f562616dcad8f91f5b2e35bcbab1
Jhon Ochoa <jhon.ochoa@openintl.com>
2024-12-14 11:10:54 -0500

    update travis

--- test/test3.py
+++ test/test3.py
@@ -5,7 +5,7 @@ class TestCalculaMedia(unittest.TestCase):
     
     def test_1(self):
         resultado = sumar(2, 4, 5 , 1)
-        self.assertNotEqual(2, resultado)
+        self.assertNotEqual(2, resultado)  
 
 if __name__ == '__main__':
     unittest.main()
